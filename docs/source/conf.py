73acad0b9522f8d49d61c2da9d4b9648283c117d
bhavin04890 <bshah@purestorage.com>
2023-05-02 16:20:57 -0400

    updating copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Portworx Enterprise'
+copyright = '2023, Portworx by Pure Storage'
+author = 'Bhavin Shah'
 
 release = '0.1'
 version = '0.1.0'
